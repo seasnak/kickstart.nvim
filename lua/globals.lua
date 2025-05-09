cb73d10a53476551a87bccf3c303d045dca4cf8b
Sasank Madineni <sasank1020@gmail.com>
2025-05-09 12:23:58 -0700

    separated config into smaller chunks to make it easier to manage

---
+++ lua/globals.lua
@@ -0,0 +1,8 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,12 +12,14 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<C-b>', ':Neotree reveal<CR>', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['<C-b>'] = 'close_window',
         },
       },
     },
