039d83ec9725e2140d22ce3730fc1248d50c5a06
Sasank Madineni <sasank1020@gmail.com>
2025-05-14 13:50:08 -0700

    starting modifying alpha config

--- lua/kickstart/plugins/alpha.lua
+++ lua/kickstart/plugins/alpha.lua
@@ -4,6 +4,14 @@
 return {
   'goolord/alpha-nvim',
   config = function()
+    local dashboard = require 'alpha.themes.dashboard'
     require('alpha').setup(require('alpha.themes.dashboard').config)
+
+    dashboard.section.buttons.val = {
+      dashboard.button('f', ' ' .. ' Find File', ':Telescope find_files <cr>'),
+      dashboard.button('n', ' ' .. ' New File', ':new <cr>'),
+      dashboard.button('r', ' ' .. ' Recent Files', ':Telescope oldfiles <cr>'),
+      dashboard.button('q', ' ' .. ' Quit', ':qall <cr>'),
+    }
   end,
 }
