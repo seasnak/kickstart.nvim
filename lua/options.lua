ca4d086602980c2731227cd1b2aab95cf35662cc
Sasank Madineni <sasank1020@gmail.com>
2025-06-07 18:45:48 -0700

    fixed typo

--- lua/options.lua
+++ lua/options.lua
@@ -46,7 +46,7 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.shiftwidth = 4
         vim.opt_local.tabstop = 4
         vim.opt_local.expandtab = true
-        vim.opt_local.autoident = true
+        vim.opt_local.autoindent = true
     end,
 })
 
